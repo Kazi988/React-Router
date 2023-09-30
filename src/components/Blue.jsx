fdef3c33b7195bc4cd278148e3eaa6be5d6a5706
Kazi <kazi98@proton.me>
2023-09-29 20:02:14 -0500

    react router

---
+++ src/components/Blue.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+export default function Blue() {
+  return (
+    <>
+      <div className="blue">
+        <h1>BLUE</h1>
+      </div>
+    </>
+  );
+}
